3a8b3caca6b61ad43a04d9a5791b8da9f9c9bceb
Saqib-002 <saqibshah2882@gmail.com>
2024-01-14 15:50:46 +0500

    added sidebars

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -64,7 +64,7 @@ function MobileNav() {
         </SheetTrigger>
         <SheetContent
           side="left"
-          className="background-light900_dark200 border-none"
+          className="background-light900_dark200 overflow-y-auto border-none"
         >
           <Link href="/" className="flex items-center gap-1">
             <Image
@@ -83,7 +83,7 @@ function MobileNav() {
               <NavContent />
             </SheetClose>
             <SignedOut>
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-3 pt-12">
                 <SheetClose asChild>
                   <Link href="/sign-in">
                     <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
