00291579b3b8f3558488046cc799acc04893d3ac
Saqib-002 <saqibshah2882@gmail.com>
2024-01-18 09:41:21 +0500

    fix eslint issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 import "./globals.css";
 
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 
 import type { Metadata } from "next";
