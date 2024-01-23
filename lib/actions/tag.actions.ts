34b388f7154b458e13bd622ff1c726f33fd7d698
Saqib-002 <saqibshah2882@gmail.com>
2024-01-23 20:37:20 +0500

    added tag page

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,7 +1,8 @@
 "use server";
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongooose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInterectedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -10,6 +11,16 @@ export async function getTopInterectedTags(params: GetTopInteractedTagsParams) {
     const user = await User.findById(userId);
     if (!user) throw new Error("User not found");
     return [{ _id: "1", name: "tag1" }];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    const tags = await Tag.find({});
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
